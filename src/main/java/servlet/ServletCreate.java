99cf7aaf0b6e5e239a98c4451f8f4989dacd61c2
AlexeiShubin <154376277+AlexeiShubin@users.noreply.github.com>
2024-07-11 21:06:32 +0300

    Add files via upload

---
+++ src/main/java/servlet/ServletCreate.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import database.Connect;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/createUser")
+public class ServletCreate extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection;
+        ResultSet resultSet;
+        int id=0;
+        try {
+            connection = Connect.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employees VALUES (?, ?, ?, ?)");
+            PreparedStatement maxID = connection.prepareStatement("SELECT MAX(id) FROM EMPLOYEES");
+
+            resultSet = maxID.executeQuery();
+
+            while (resultSet.next()) id = resultSet.getInt("max");
+            int newID=id+1;
+
+            preparedStatement.setInt(1, newID);
+            preparedStatement.setString(2, req.getParameter("name"));
+            preparedStatement.setString(3, req.getParameter("surname"));
+            preparedStatement.setString(4, req.getParameter("phone"));
+            preparedStatement.executeUpdate();
+            getServletContext().getRequestDispatcher("/endCreate.jsp").forward(req, resp);
+
+        } catch (Exception e) {
+            getServletContext().getRequestDispatcher("/error.jsp").forward(req, resp);
+        }
+
+    }
+}
---
+++ src/main/java/servlet/ServletGet.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import database.Connect;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet("/getUser")
+public class ServletGet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/get.jsp").forward(req, resp);
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection;
+        ResultSet resultSet;
+        int id=Integer.parseInt(req.getParameter("id"));
+        if (id<0){
+            getServletContext().getRequestDispatcher("/error.jsp").forward(req, resp);
+        }
+
+        try {
+            connection= Connect.getInstance().getConnection();
+            PreparedStatement preparedStatement=connection.prepareStatement("SELECT * FROM employees WHERE id=?");
+            preparedStatement.setInt(1, id);
+            resultSet=preparedStatement.executeQuery();
+            while (resultSet.next()){
+                req.setAttribute("id", resultSet.getString("id"));
+                req.setAttribute("name", resultSet.getString("name"));
+                req.setAttribute("surname", resultSet.getString("surname"));
+                req.setAttribute("phone", resultSet.getString("phone"));
+            }
+            getServletContext().getRequestDispatcher("/endGet.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            getServletContext().getRequestDispatcher("/error.jsp").forward(req, resp);
+        }
+    }
+}
