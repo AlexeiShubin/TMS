04f4371bddb05c343d7ef7ef72d515683181e083
AlexeiShubin <154376277+AlexeiShubin@users.noreply.github.com>
2024-07-27 02:56:05 +0300

    Add files via upload

---
+++ src/main/java/DZ/controller/JDBCController.java
@@ -0,0 +1,143 @@
+package DZ.controller;
+
+
+import DZ.connect.ConnectSQL;
+import DZ.examination.Examination;
+import DZ.user.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Controller
+public class JDBCController {
+    @GetMapping("/create")
+    public String createPage() {
+        return "create";
+    }
+
+    @GetMapping("/createUser")
+    public String createUser(User user) {
+        try {
+            Connection connection = ConnectSQL.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employees VALUES (?, ?, ?, ?)");
+            PreparedStatement maxID = connection.prepareStatement("SELECT MAX(id) FROM EMPLOYEES");
+
+            ResultSet resultSet = maxID.executeQuery();
+
+            int id = 0;
+            while (resultSet.next()) id = resultSet.getInt("max");
+
+            preparedStatement.setInt(1, id + 1);
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getSurname());
+            preparedStatement.setString(4, user.getPhone());
+            preparedStatement.executeUpdate();
+
+            return "createEnd";
+        } catch (SQLException e) {
+            return "error";
+        }
+    }
+
+    @GetMapping("/get")
+    public String getPage() {
+        return "get";
+    }
+
+    @GetMapping("/getUser")
+    public String getUser(@RequestParam(value = "id", required = false) String idStr, Model model) {
+
+        if (idStr==null||idStr.isEmpty()||!Examination.isDigit(idStr)){
+            return "error";
+        }
+
+        int id=Integer.parseInt(idStr);
+
+        if (id <= 0) {
+            return "error";
+        }
+
+        try {
+            Connection connection = ConnectSQL.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employees WHERE id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                model.addAttribute("id", resultSet.getString("id"));
+                model.addAttribute("name", resultSet.getString("name"));
+                model.addAttribute("surname", resultSet.getString("surname"));
+                model.addAttribute("phone", resultSet.getString("phone"));
+            }
+
+            return "getEnd";
+        } catch (SQLException e) {
+            return "error";
+        }
+    }
+
+    @GetMapping("/change")
+    public String changePage(){
+        return "change";
+    }
+
+    @GetMapping("/changeUser")
+    public String changeUser(@RequestParam(value = "id", required = false) String idStr, @RequestParam(value = "name", required = false) String newFirstName){
+
+        if (idStr==null||idStr.isEmpty()||!Examination.isDigit(idStr)){
+            return "error";
+        }
+
+        int id=Integer.parseInt(idStr);
+
+        if (id <= 0) {
+            return "error";
+        }
+
+        try {
+            Connection connection=ConnectSQL.getInstance().getConnection();
+            PreparedStatement preparedStatement=connection.prepareStatement("UPDATE EMPLOYEES SET name = ? WHERE id = ?");
+            preparedStatement.setInt(2, id);
+            preparedStatement.setString(1, newFirstName);
+            preparedStatement.executeUpdate();
+            return "changeEnd";
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @GetMapping("/delete")
+    public String deletePage(){
+        return "delete";
+    }
+
+    @GetMapping("/deleteUser")
+    public String deleteUser(@RequestParam(value = "id", required = false) String idStr, Model model){
+
+        if (idStr==null||idStr.isEmpty()||!Examination.isDigit(idStr)){
+            return "error";
+        }
+
+        int id=Integer.parseInt(idStr);
+
+        if (id <= 0) {
+            return "error";
+        }
+
+        try {
+            Connection connection=ConnectSQL.getInstance().getConnection();
+            PreparedStatement preparedStatement=connection.prepareStatement("DELETE FROM EMPLOYEES WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+            model.addAttribute("name", id);
+            return "deleteEnd";
+        } catch (SQLException e) {
+            return "error";
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/create.jsp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="createUser" method="get" class="form-example">
+    <div class="form-example">
+        <label for="first_name">Enter First Name of employee:</label>
+        <input type="text" name="name" id="name"/>
+        <br>
+        <label for="last_name">Enter Last Name of employee: </label>
+        <input type="text" name="surname" id="surname"/>
+        <br>
+        <label for="phone_number">Enter Phone Number of employee: </label>
+        <input type="text" name="phone" id="phone"/>
+        <br>
+        <button type="submit">ADD EMPLOYEE</button>
+
+    </div>
+</form>
+</body>
+</html>
