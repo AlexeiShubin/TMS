a625c179043d8f129cd71d2e38dfa546a5426a47
AlexeiShubin <154376277+AlexeiShubin@users.noreply.github.com>
2024-08-06 18:43:26 +0300

    Update ServletStart.java

--- src/main/java/servlet/ServletStart.java
+++ src/main/java/servlet/ServletStart.java
@@ -9,10 +9,8 @@ import java.io.IOException;
 
 public class ServletStart extends HttpServlet {
 
-    public static boolean isInitialized=false;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        isInitialized=false;
         getServletContext().getRequestDispatcher("webapp/index.jsp").forward(req, resp);
     }
 }
