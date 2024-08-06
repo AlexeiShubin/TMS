bec54fafcc2bab87bb23e3ff71b826f46ec87cfe
AlexeiShubin <154376277+AlexeiShubin@users.noreply.github.com>
2024-08-06 18:55:57 +0300

    Update ServletGet.java

--- src/main/java/servlet/ServletGet.java
+++ src/main/java/servlet/ServletGet.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 public class ServletGet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Connection connection;
         ResultSet resultSet;
