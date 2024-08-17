0309ae0912b6873d03634b912ae6383fdc7ea0d4
AlexeiShubin <154376277+AlexeiShubin@users.noreply.github.com>
2024-08-17 16:01:41 +0300

    Add files via upload

---
+++ src/main/java/app/connect/Connect.java
@@ -0,0 +1,36 @@
+package app.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static volatile Connect instance;
+    private static final String URL = "jdbc:postgresql://localhost:5432/DZ35";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "64Tank46";
+
+    private Connect() {
+        try {
+            Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static Connect getInstance() {
+        if (instance == null) {
+            synchronized (Connect.class) {
+                if (instance == null) {
+                    instance = new Connect();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/jsp/getEnd.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>DATABASE_INFO</title>
+</head>
+<body>
+<p>Данные пользователя с id = ${id}</p>
+<p>EMPLOYEE_ID: ${id}</p>
+<p>FIRST_NAME: ${name}</p>
+<p>LAST_NAME: ${surname}</p>
+<p>PHONE_NUMBER: ${phone}</p>
+</body>
+</html>
